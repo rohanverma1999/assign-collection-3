e174c4e92c321d83f838f4224a67847a634e2a67
Rohan Verma <vermarohan1022@gmail.com>
2018-10-12 12:06:07 +0530

    ass

---
+++ sort1.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+ class Sort1
+
+ {
+
+
+
+	public static void main(String[] args) 
+
+     {
+
+     ArrayList<String> s=new ArrayList<String>();
+
+     s.add("Nalin");
+
+     s.add("Ayush");
+
+     s.add("Mukul");
+
+     Collections.sort(s);
+
+     System.out.println(s);
+
+
+
+     ArrayList<Integer> i=new ArrayList<Integer>();
+
+
+
+     i.add(1);
+
+     i.add(22);
+
+     i.add(3);
+
+     Collections.sort(i);
+
+     System.out.println(i);
+
+	}
+
+
+
+}
